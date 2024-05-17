7af789a4e4ae71b4f167995963ec399099942945
Mahika Nair <145975620+mahikanair@users.noreply.github.com>
2024-05-17 23:02:13 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,42 @@
 from flask import Flask, request, jsonify
-import joblib
 import numpy as np
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load('random_forest_model.joblib')
+data = pd.read_csv('Crop_Recommendation.csv')
+# Separate the features and the target variable
+feature_columns = ['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall']
+X = data[feature_columns]
+y = data['Crop']
+
+# Standardize the features
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+# Initialize the Random Forest Classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X, y)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get the features from the request
-        data = request.get_json()
-        features = np.array([data['features']])
+        features = request.get_json()
+        features_df = pd.DataFrame([features], columns=feature_columns)
         
-        # Make a prediction
-        prediction = model.predict(features)
+        features_scaled = scaler.transform(features_df)
+        prediction = model.predict(features_scaled)
         
         # Return the prediction as a JSON response
         return jsonify({'prediction': int(prediction[0])})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
