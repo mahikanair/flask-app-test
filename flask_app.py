7b6aaf243218498f32cb8619f31ea01e45487906
Mahika Nair <145975620+mahikanair@users.noreply.github.com>
2024-05-19 18:37:11 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,38 +1,26 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+import joblib
 
 app = Flask(__name__)
-
-# Load the trained model
-data = pd.read_csv('Crop_Recommendation.csv')
 # Separate the features and the target variable
 feature_columns = ['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall']
-X = data[feature_columns]
-y = data['Crop']
-
-# Standardize the features
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-# Initialize the Random Forest Classifier
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X, y)
+# Load the trained model and scaler
+model = joblib.load('random_forest_model.joblib')
+scaler = joblib.load('scaler.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the features from the request
     features = request.get_json()
     features_df = pd.DataFrame([features], columns=feature_columns)
-    
     features_scaled = scaler.transform(features_df)
     prediction = model.predict(features_scaled)
     
     # Return the prediction as a JSON response
     return jsonify({'prediction': prediction[0]})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
